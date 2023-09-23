8544777aa948be51c80bb2dc900780c72cbf0da4
Neo Masilo <neolawrencemasilo@gmail.com>
2023-09-23 09:16:40 +0000

    bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts elements by comparing
+ *		the current element with the one after it
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp;
+	char *swapped = "false";
+
+	if (array == NULL)
+		return;
+
+	if (size > 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			for (j = 0; j < (size - 1 - i); j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+					swapped = "true";
+					print_array(array, size);
+				}
+			}
+			if (strcmp(swapped, "false") == 0)
+				break;
+		}
+	}
+}
